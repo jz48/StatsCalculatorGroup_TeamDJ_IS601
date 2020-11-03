f78ae0f922ef83acd2720ae6024563a8cca5b8aa
Krishna Eleti <krishnaeleti@gmail.com>
2020-11-03 18:34:16 -0500

    file path issues in testing 3

--- src/PopSampleTest.py
+++ src/PopSampleTest.py
@@ -1,11 +1,12 @@
-from src.PopSample import cochran, find_sample_size, margin, conf_int
+from Calc import Calculator
+from PopSample import PopulationSample, cochran, find_sample_size, margin, conf_int
 
-
-class PopSampling():
+class MyPopSampleTestCase(Calculator):
     data = []
 
-    def __init__(self):
-        super().__init__()
+    def test_instantiate_PopulationSample(self):
+        self.assertIsInstance(self.PopulationSample, PopulationSample)
+
 
     def cochran(self, z, p, q, e):
         self.result = cochran(z, p, q, e)
