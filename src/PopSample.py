22fc6d936b589ab0d7c98b33113f9b62a740cd45
Krishna Eleti <krishnaeleti@gmail.com>
2020-11-03 17:26:33 -0500

    task: conf int and sample size added

--- src/PopSample.py
+++ src/PopSample.py
@@ -51,3 +51,72 @@ def find_sample_size(p, q, za2, e):
         print('Error! Cannot divide by 0')
     except ValueError:
         print('Error! Invalid data entry')
+
+        def margin(a, b, c, d, e):
+            try:
+                a = int(a)
+                b = int(b)
+                c = int(c)
+                d = int(d)
+                e = int(e)
+                data = [a, b, c, d, e]
+
+                n = len(data)
+                stand_dev = standard_deviation(a, b, c, d, e)
+                sqr_n = round(squared_root(n), 2)
+                value1 = stand_dev / sqr_n
+                z = int(1.96)
+                result = round((value1 * z), 1)
+                return result
+
+            except ZeroDivisionError:
+                print("Error! Cannot Divide by 0")
+            except ValueError:
+                print("Error! Invalid data inputs")
+
+def margin(a, b, c, d, e):
+    try:
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        d = int(d)
+        e = int(e)
+        data = [a, b, c, d, e]
+
+        n = len(data)
+        stand_dev = standard_deviation(a, b, c, d, e)
+        sqr_n = round(square_root(n), 2)
+        value1 = stand_dev / sqr_n
+        z = int(1.96)
+        result = round((value1 * z), 1)
+        return result
+
+    except ZeroDivisionError:
+        print("Error! Cannot Divide by 0")
+    except ValueError:
+        print("Error! Invalid data inputs")
+
+def conf_int(a, b, c, d, e):
+    try:
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        d = int(d)
+        e = int(e)
+        data = [a, b, c, d, e]
+
+        n = len(data)
+        z = int(1.96)
+        x = mean(a, b, c, d, e)
+        s = standard_deviation(a, b, c, d, e)
+
+        num1 = squared_root(n)
+        num2 = s / num1
+        num3 = z * num2
+        result = round((x - num3), 2)
+        return result
+
+    except ZeroDivisionError:
+        print("Error! Cannot Divide by 0")
+    except ValueError:
+        print("Error! Invalid data inputs")
--- src/PopSampleTest.py
+++ src/PopSampleTest.py
@@ -1,5 +1,5 @@
 from .Calc import Calculator
-from .PopSample import cochran, find_sample_size
+from .PopSample import cochran, find_sample_size, margin, conf_int
 
 
 class PopSampling(Calculator):
@@ -15,3 +15,13 @@ class PopSampling(Calculator):
     def find_sample_size(self, p, q, za2, e):
         self.result = find_sample_size(p, q, za2, e)
         return self.result
+
+    def margin(self, a, b, c, d, e):
+        self.result = margin(a, b, c, d, e)
+        return self.result
+
+    def conf_int(self, a, b, c, d, e):
+        self.result = conf_int(a, b, c, d, e)
+        return self.result
+
+    pass
