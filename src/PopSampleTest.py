e0b54c568704c651c99ffe695358f82770c4a535
Krishna Eleti <krishnaeleti@gmail.com>
2020-11-03 20:48:41 -0500

    fix: added tests for population sample

--- src/PopSampleTest.py
+++ src/PopSampleTest.py
@@ -1,28 +1,45 @@
 import unittest
-from Calc import Calculator
-from PopSample import PopulationSample, cochran, find_sample_size, margin, conf_int
+from PopSample import PopulationSample
+from CsvReader import CsvReader
 
-class Popsampling(Calculator):
+class MyTestCase(unittest.TestCase):
 
-    def __init__(self):
-        super().__init__()
+    def setUp(self):
+        self.PopulationSample = PopulationSample()
 
+    # To test instantiation of PopSampling class
+    def test_instantiate_statistics_calculator(self):
+        self.assertIsInstance(self.PopulationSample, PopulationSample)
 
-    def cochran(self, z, p, q, e):
-        self.result = cochran(z, p, q, e)
-        return self.result
+    # Testing Cochran
+    def test_cochran(self):
+        test_data = CsvReader('./src/data/Cochran_Data.csv').data
+        for row in test_data:
+            #pprint(self.PopSampling.cochran(row['Z'], row['p'], row['q'], row['e']))
+            self.assertEqual(self.PopulationSample.cochran(row['Z'], row['p'], row['q'], row['e']), int(row['Sample']))
+            self.assertEqual(self.PopulationSample.result, int(row['Sample']))
+        test_data.clear()
 
-    def find_sample_size(self, p, q, za2, e):
-        self.result = find_sample_size(p, q, za2, e)
-        return self.result
+    # Testing FindSample
+    def test_find_sample_size(self):
+        test_data = CsvReader('./src/data/FindSample_Data.csv').data
+        for row in test_data:
+            self.assertEqual(self.PopulationSample.find_sample_size(row['P'], row['q'], row['za2'], row['e']), int(row['Sample']))
+            self.assertEqual(self.PopulationSample.result, int(row['Sample']))
+        test_data.clear()
 
-    def margin(self, a, b, c, d, e):
-        self.result = margin(a, b, c, d, e)
-        return self.result
+    # Testing MarginOfError
+    def test_margin(self):
+        test_data = CsvReader('./src/data/Margin_Data.csv').data
+        for row in test_data:
+            self.assertEqual(self.PopulationSample.margin(row['Value1'], row['Value2'], row['Value3'], row['Value4'], row['Value5']), float(row['Error']))
+            self.assertEqual(self.PopulationSample.result, float(row['Error']))
+        test_data.clear()
 
-    def conf_int(self, a, b, c, d, e):
-        self.result = conf_int(a, b, c, d, e)
-        return self.result
-
-if __name__ == '__main__':
-    unittest.main()
+    # Testing ConfidenceInterval
+    def test_conf_int(self):
+        test_data = CsvReader('./src/data/ConfInt_Data.csv').data
+        for row in test_data:
+            self.assertEqual(self.PopulationSample.conf_int(row['a'], row['b'], row['c'], row['d'], row['e']), float(row['ConfInt']))
+            self.assertEqual(self.PopulationSample.result, float(row['ConfInt']))
+        test_data.clear()
